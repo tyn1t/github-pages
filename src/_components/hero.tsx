1e21d6faed23b534acc7e2a9b2096c94cafead03
Giovanni <anderlansouza71@gmail.com>
2025-03-06 10:25:20 -0400

    fota-dog

--- src/_components/hero.tsx
+++ src/_components/hero.tsx
@@ -1,6 +1,6 @@
 import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
 import CatHero from "./../../public/cat-hero.png"
-import Dog from "./../../public/hero-dog.png"
+import Dog from "./../../public/bg-hero.png"
 
 import Image from 'next/image'
 
