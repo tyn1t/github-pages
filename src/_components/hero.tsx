b8fba8ae627e15084d2e075f398897aefd19a5cc
Giovanni <anderlansouza71@gmail.com>
2025-03-06 11:04:09 -0400

    fot

--- src/_components/hero.tsx
+++ src/_components/hero.tsx
@@ -8,14 +8,15 @@ export function Hero() {
     return (
         <section className="bg-[#2148ca]  text-white relative overflow-hidden">
             <div>
-            <Image
-                src={"/foto-hero.webp"}
-                alt='imagem de dog'
-                className='object-cover opacity-60 lg:hidden'
-                fill
-                priority
-                sizes=""
-            />
+                <Image
+                    src="/foto-hero.webp"
+                    alt="imagem de dog"
+                    className="object-cover opacity-60 lg:hidden"
+                    fill
+                    priority
+                    unoptimized 
+                />
+
             <div className="absolute inset-0 bg-black opacity-40 md:hidden"></div>
             </div>
             <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative" >
