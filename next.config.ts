90edc501c704a77f6e393ca3f84123a9c92bc6e3
Giovanni <anderlansouza71@gmail.com>
2025-03-06 11:37:18 -0400

    foto

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Exporta como site estático
-  images: {
-    unoptimized: true, // Evita problemas com imagens no GitHub Pages
-  },
   basePath: "/tyn1t",
   assetPrefix: "/Pets-landing-page/",
 };
