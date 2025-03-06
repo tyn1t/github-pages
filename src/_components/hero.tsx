36070cfa32bbe1a40998aff1b328a5f6aa204247
Giovanni <anderlansouza71@gmail.com>
2025-03-06 10:40:01 -0400

    fotadg

--- src/_components/hero.tsx
+++ src/_components/hero.tsx
@@ -9,7 +9,7 @@ export function Hero() {
         <section className="bg-[#2148ca]  text-white relative overflow-hidden">
             <div>
             <Image
-                src={"/bg-hero.png"}
+                src={"./bg-hero.png"}
                 alt='imagem de dog'
                 className='object-cover opacity-60 lg:hidden'
                 fill
