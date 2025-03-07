917c162926fe8c2920c0256385c9e861fe200eab
Giovanni <anderlansouza71@gmail.com>
2025-03-07 02:27:04 -0400

    Deploy Next.js 15 no GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV == "production";
+
 const nextConfig: NextConfig = {
   output: 'export', 
-//   images: {
-//     unoptimized: true,
-//   },
-//   basePath: "/tyn1t",
-//   assetPrefix: "/Pets-landing-page/",
+  distDir:'dist',
+  images: {
+      unoptimized: true,
+    },
+  basePath: isProd ? "/Pets-landing-page" : ""
 };
 
 
