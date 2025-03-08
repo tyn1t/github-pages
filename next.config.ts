39c50b82cc135778b884188062d403b1593f0277
Giovanni <anderlansouza71@gmail.com>
2025-03-08 11:44:19 -0400

    confil

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  basePath: '/pets-landing-page',
+  basePath: '/pets-landing-page', // Nome do repositório
   images: {
-    unoptimized: true, 
+    unoptimized: true, // Necessário para imagens no GitHub Pages
   },
+};
+
+export default nextConfig;
   // images: {
   //   unoptimized: true,
   // },
   // basePath: process.env.NODE_ENV == "production" ? "/Pets-landing-page" : "/Pets-landing-page" ,
   // experimental: { optimizeCss: true }, 
-  
-}
-
-
-export default nextConfig;
