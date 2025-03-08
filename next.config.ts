e0ee3ff01dd3d2e3771daf4062b1c48685607a0d
Giovanni <anderlansouza71@gmail.com>
2025-03-08 10:56:57 -0400

    config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Pets-landing-page',
+  basePath: '/pets-landing-page',
   images: {
     unoptimized: true, 
   },
