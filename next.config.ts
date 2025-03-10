eb0cc46351da4bd09f5ca9442f6f5338554e7fc3
Giovanni <anderlansouza71@gmail.com>
2025-03-10 13:48:32 -0400

    erro css e img

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/github-pages',
+  assetPrefix: '/github-pages',
+  output: 'export',
+  images: {
+    domains: ["tyn1t.github.io"] 
+  },
+};
+
+export default nextConfig;
