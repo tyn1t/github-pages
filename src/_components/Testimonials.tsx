9194add07d343f33524fc39eca0b8a5e71d265cd
Giovanni <anderlansouza71@gmail.com>
2025-03-10 19:58:35 -0400

    im teste

--- src/_components/Testimonials.tsx
+++ src/_components/Testimonials.tsx
@@ -69,7 +69,7 @@ export function Testimonials () {
                                     <div key={index} className="flex-[0_0_100%] min-w-0 px-3">
                                         <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
                                             <div className="flex flex-col items-center text-center space-y-4">
-                                                <div className="relative w-24 h-24">
+                                                <div className="relative w-24 h-24 rounded-full">
                                                     <Image
                                                         src={item.img}
                                                         alt={item.author}
